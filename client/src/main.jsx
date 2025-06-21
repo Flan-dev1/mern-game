19bca4774a2669fbb4c577bb2d75ae946473e1f2
Flan-dev1 <clarencem.123@gmail.com>
2025-06-21 16:10:39 +0800

    feat: integrate react-router for navigation and update App component structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Users from "./components/users";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Users />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
